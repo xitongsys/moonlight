90c97dd33b8cb021e5e33df4c72042700669c88c
xitongsys <xitongsys@gmail.com>
2022-03-31 14:13:55 +0800

    ..

---
+++ moonlightpy/__main__.py
@@ -0,0 +1,18 @@
+import sys
+import server, client
+
+if __name__ == '__main__':
+    role = sys.argv[1]
+
+    if role == 'server':
+        cfg_file = sys.argv[2]
+        s = server.Server(cfg_file)
+        s.start()
+
+    elif role == 'client':
+        addr, port = sys.argv[2], int(sys.argv[3])
+        c = client.Client(addr, port)
+        c.start()
+
+    else:
+        raise ValueError("unknown role {}".format(role))
