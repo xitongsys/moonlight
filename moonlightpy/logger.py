b553842d478ea7fc578a6f5fb7bd5ec80847b018
xitongsys <xitongsys@gmail.com>
2022-04-02 09:50:57 +0800

    fix logger

--- moonlightpy/logger.py
+++ moonlightpy/logger.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 LOG_FORMAT = "[%(asctime)s][%(levelname)s] %(message)s"
 logging.basicConfig(
-    filename="laurel_{}.log".format(datetime.now().strftime("%Y%m%d_%H%M")),
+    filename="moonlight_{}.log".format(datetime.now().strftime("%Y%m%d")),
     level=logging.INFO,
     format=LOG_FORMAT,
 )
